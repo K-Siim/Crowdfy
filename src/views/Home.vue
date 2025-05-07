c7309005b6eabdcb37303606ad7599168dbfe64e
K-Siim <kevinsiim007@gmail.com>
2025-05-07 13:06:01 +0300

    Full vue-tailwind-router-setup

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+
+</script>
+
+<template>
+    <div class="text-2xl text-blue-600">Tere tulemast!</div>
+    <Button>Button</Button>
+
+  </template>
+  
